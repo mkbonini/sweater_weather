2e9c6f22b6f518fa70a1b9515754ba0653131fee
mkbonini <mkbonini@gmail.com>
2022-09-27 12:18:10 -0600

    refactor: user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
+  validates :api_key, uniqueness: true, presence: true
   validates_presence_of :password
 
   has_secure_password
