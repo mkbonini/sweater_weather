665d8d09747847a166fee3aca3e652906579e9b3
mkbonini <mkbonini@gmail.com>
2022-09-27 15:33:46 -0600

    feat: user show when authenitcated

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,15 @@ class Api::V1::UsersController < ApplicationController
     render json: UserSerializer.new( user = User.create!(user_params) )
   end
 
+  def show  
+    user = User.find_by(email: user_params[:email])
+    if user.authenticate(user_params[:password])
+      render json: UserSerializer.new( user )
+    else
+      binding.pry
+    end
+  end
+
   private
   def user_params
     params[:user].permit(:email, :password, :password_confirmation)
